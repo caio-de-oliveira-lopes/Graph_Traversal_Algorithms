d9dc44af8b3ee2aaaa23987993b922afedacd662
caio-de-oliveira-lopes <caio.oliveiracvt@gmail.com>
2023-11-19 15:47:01 -0300

    Added graph with both methods DFS and BFS (only with adjacency lists)

---
+++ GraphTraversal/Graph.cs
@@ -0,0 +1,82 @@
+﻿namespace GraphTraversal
+{
+    public class Graph
+    {
+        private int NumberOfVertices { get; set; }
+        private GraphOrientation Orientation { get; set; }
+        private LinkedList<int>[] AdjacencyLists { get; set; }
+
+        public Graph(int v, GraphOrientation orientation)
+        {
+            NumberOfVertices = v;
+            Orientation = orientation;
+            AdjacencyLists = new LinkedList<int>[v];
+            for (int i = 0; i < AdjacencyLists.Length; i++)
+                AdjacencyLists[i] = new();
+        }
+
+        public enum GraphOrientation
+        {
+            Bidirected,
+            Directed
+        }
+
+        public void AddEdge(int v, int w)
+        {
+            if (!AdjacencyLists[v].Contains(w))
+                AdjacencyLists[v].AddLast(w);
+
+            if (Orientation == GraphOrientation.Bidirected && !AdjacencyLists[w].Contains(v))
+                AdjacencyLists[w].AddLast(v);
+        }
+
+        public void BFS(int s)
+        {
+            bool[] visited = new bool[NumberOfVertices];
+            for (int i = 0; i < NumberOfVertices; i++)
+                visited[i] = false;
+
+            LinkedList<int> queue = new();
+
+            visited[s] = true;
+            queue.AddLast(s);
+
+            while (queue.Any())
+            {
+                s = queue.First();
+                Console.Write(s + " ");
+                queue.RemoveFirst();
+
+                LinkedList<int> list = AdjacencyLists[s];
+
+                foreach (int val in list)
+                {
+                    if (!visited[val])
+                    {
+                        visited[val] = true;
+                        queue.AddLast(val);
+                    }
+                }
+            }
+        }
+
+        private void DFSUtil(int v, bool[] visited)
+        {
+            visited[v] = true;
+            Console.Write(v + " ");
+
+            LinkedList<int> vList = AdjacencyLists[v];
+            foreach (int n in vList)
+            {
+                if (!visited[n])
+                    DFSUtil(n, visited);
+            }
+        }
+
+        public void DFS(int v)
+        {
+            bool[] visited = new bool[NumberOfVertices];
+            DFSUtil(v, visited);
+        }
+    }
+}
---
+++ GraphTraversal/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace GraphTraversal
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Graph g = new Graph(4, Graph.GraphOrientation.Bidirected);
+            g.AddEdge(0, 1);
+            g.AddEdge(0, 2);
+            g.AddEdge(1, 2);
+            g.AddEdge(2, 0);
+            g.AddEdge(2, 3);
+            g.AddEdge(3, 3);
+
+            Console.WriteLine("Following is Breadth First "
+                          + "Traversal(starting from "
+                          + "vertex 2)");
+            g.BFS(2);
+
+            Console.WriteLine(
+            "\n\nFollowing is Depth First Traversal "
+            + "(starting from vertex 2)");
+
+            // Function call
+            g.DFS(2);
+        }
+    }
+}
